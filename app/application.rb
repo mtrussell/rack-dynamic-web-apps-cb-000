e1b1cbef6fcfbe6970e9d6a25458d562c0e2d9c4
Matt Trussell <smtrussell@gmail.com>
2018-02-15 03:25:06 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    
+
 
     num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
@@ -12,9 +12,10 @@ class Application
       resp.write "Your number are #{num_1}, #{num_2}, and #{num_3}!"
       resp.write "You Win"
     else
+      resp.write "Your number are #{num_1}, #{num_2}, and #{num_3}!"
       resp.write "You Lose"
     end
-    
+
     resp.finish
   end
 
