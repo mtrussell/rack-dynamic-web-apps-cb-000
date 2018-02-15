d4682a718624a89a19ac6347cd45965d9d4fdef2
Matt Trussell <smtrussell@gmail.com>
2018-02-15 03:29:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,11 +8,13 @@ class Application
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
 
+    resp.write "#{num_1}\n"
+    resp.write "#{num_2}\n"
+    resp.write "#{num_3}\n"
+
     if num_1 == num_2 && num_2 == num_3
-      resp.write "Your number are #{num_1}, #{num_2}, and #{num_3}! "
       resp.write "You Win"
     else
-      resp.write "Your number are #{num_1}, #{num_2}, and #{num_3}! "
       resp.write "You Lose"
     end
 
